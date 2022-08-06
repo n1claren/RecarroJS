33fc4bce759a6287b6b5a038a266293c516d5a5d
Nikolay Ivanov <65788380+n1claren@users.noreply.github.com>
2022-08-06 20:38:13 +0300

    implemented react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import { Routes, Route, useNavigate} from 'react-router-dom';
+
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Homepage from "./components/Homepage";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import ListCar from "./components/ListCar";
 
 function App() {
     return (
         <div className="App">
             <Header />
-            <Homepage />
 
+            <Routes>
+                <Route path="/" element={<Homepage />} />
+                <Route path="/Login" element={<Login />} />
+                <Route path="/Register" element={<Register />} />
+                <Route path="/ListCar" element={<ListCar />} />
+            </Routes>
+            
             <Footer />
         </div>
     );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,36 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
     return (
         <header className="header">
             <h1>
-                <p>Recarro</p>
+                <p>
+                    <Link to="/">
+                        Recarro®
+                    </Link>
+                </p>
             </h1>
 
             <nav>
-                <a href="#">All Cars</a>
-                <div id="user">
-                    <a href="#">List a car</a>
-                    <a href="#">Logout</a>
-                </div>
-                <div id="guest">
-                    <a href="#">Login</a>
-                    <a href="#">Register</a>
-                </div>
+                <Link to="AllCars">
+                    All Cars
+                </Link>
+
+                <Link to="ListCar">
+                    List a car
+                </Link>
+
+                <Link to="Logout">
+                    Logout
+                </Link>
+
+                <Link to="Login">
+                    Login
+                </Link>
+
+                <Link to="Register">
+                    Register
+                </Link>
             </nav>
         </header>
     );
