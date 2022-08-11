4c0e6c477d9c6aaa2fd9707a3f6df8fcc0eeaf85
Nikolay Ivanov <65788380+n1claren@users.noreply.github.com>
2022-08-11 22:44:17 +0300

    connected register to server

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,9 +1,37 @@
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+ 
+import * as authService from "../services/authService";
+import { AuthContext } from "../contexts/AuthContext";
 
 const Register = () => {
+    const { userLoginHandler } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const onSubmit = (ev) => {
+        ev.preventDefault();
+
+        const formData = new FormData(ev.target);
+
+        const email = formData.get('email');
+        const password = formData.get('password');
+        const confirmPassword = formData.get('confirm-password');
+
+        if (password !== confirmPassword) {
+            return;
+        }
+
+        authService.register(email, password)
+            .then(authData => {
+                userLoginHandler(authData);
+                navigate("/");
+            });
+    }
+
     return (
         <section id="register-page" className="content auth">
-            <form id="register">
+            <form id="register" onSubmit={onSubmit}>
                 <div className="container">
                     <h1>Register</h1>
                     <label htmlFor="email">Email:</label>
